b031efd6a83649658931d3f60fcf4952ab2e66e6
Aline Chiang <alinechiang17@gmail.com>
2023-09-18 21:49:30 -0400

    step 2

--- app/src/main/java/com/cst7335/jian0133/MainActivity.java
+++ app/src/main/java/com/cst7335/jian0133/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
 
         Button clickHereButton = findViewById(R.id.clickHereButton);
         Switch switchButton = findViewById(R.id.switchButton);
