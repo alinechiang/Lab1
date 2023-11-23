b15bf03be98c2284b2de0af5e796e05bfebfdc08
Aline Chiang <alinechiang17@gmail.com>
2023-11-23 17:57:57 -0500

    finished steps 1-5

--- app/src/main/java/com/cst7335/jian0133/MainActivity.java
+++ app/src/main/java/com/cst7335/jian0133/MainActivity.java
@@ -1,10 +1,13 @@
 package com.cst7335.jian0133;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
@@ -14,43 +17,28 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String PREFS_NAME = "PrefsFile";
+    private EditText emailEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative;
+        setContentView(R.layout.activity_profile);
 
-        Button clickHereButton = findViewById(R.id.clickHereButton);
-        Switch switchButton = findViewById(R.id.switchButton);
-        View layout = findViewById(R.id.layout);
+        emailEditText = findViewById(R.id.editTextEmail);
 
-        clickHereButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String toastMessage = getResources().getString(R.string.toast_message);
-                Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_LONG).show();
-            }
-        });
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME,MODE_PRIVATE);
+        String email = settings.getString("email","");
+        emailEditText.setText(email);
+    }
 
-        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                String state = isChecked ? "on" : "off";
-                String snackbarMessage = "The switch is now " + state;
+    @Override
+    protected void onPause() {
+        super.onPause();
 
-                Snackbar snackbar = Snackbar.make(layout, snackbarMessage, Snackbar.LENGTH_LONG);
-
-                if (isChecked) {
-                    snackbar.setAction("Undo", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            // Perform the action to revert the switch state (if needed)
-                            switchButton.setChecked(false); // For example, set it back to off
-                        }
-                    });
-                }
-
-                snackbar.show();
-            }
-        });
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("email",emailEditText.getText().toString());
+        editor.apply();
     }
 }
